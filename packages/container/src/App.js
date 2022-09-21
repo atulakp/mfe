525b5c0a98a7d04c99f333196782cbf606b58c86
AKP <akphbk619@gmail.com>
2022-09-21 19:31:27 +0530

    Changes for prod style fix

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,13 +1,26 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
 import MarketingApp from "./components/MarketingApp";
+import Header from "./components/Header";
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: "co",
+});
 
 function App(props) {
   return (
-    <div>
-      Hi !!Container - Host App
-      <hr />
-      <MarketingApp />
-    </div>
+    <BrowserRouter>
+      <StylesProvider createGenerateClassName={generateClassName}>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </StylesProvider>
+    </BrowserRouter>
   );
 }
 
