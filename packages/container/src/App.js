91fb9acefe83b015f42138efe0c80b4087f67f6a
AKP <akphbk619@gmail.com>
2022-09-21 13:04:19 +0530

    Updated text 2

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from "./components/MarketingApp";
 function App(props) {
   return (
     <div>
-      Hi !! from Container App and passing marketing App
+      Hi !!! from Container App and passing marketing App
       <hr />
       <MarketingApp />
     </div>
